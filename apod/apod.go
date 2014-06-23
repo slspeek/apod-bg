14b085ccce094efd1606fd67c8a47bdc3d57ff31
Frank Willemsen <frankwillemsen@yahoo.com>
2014-06-23 15:21:25 +0200

    Method Now added.
    Change-Id: Iba6269d9fd9f3558c10075e2e4b009bd860dfafc

--- apod/apod.go
+++ apod/apod.go
@@ -4,6 +4,14 @@ import (
 	"github.com/101loops/clock"
 )
 
+const format = "20060102"
+
 type APOD struct {
 	Clock clock.Clock
+}
+
+// Now retutns a string with the date in ISO format.
+func (a *APOD) Now() string {
+  t := a.Clock.Now()
+  return t.Format(format) 
 }
---
+++ apod/apod_test.go
@@ -0,0 +1,19 @@
+package apod
+
+import (
+  "github.com/101loops/clock"
+  "testing"
+  "time"
+)
+
+func TestNow(t *testing.T) {
+  t0 := time.Date(2014,1,21,0,0,0,0,time.UTC)
+  iso := "20140121"
+  m := clock.NewMock()
+  m.Set(t0)
+
+  apod := APOD{m}
+  if apod.Now() != iso {
+    t.Errorf("Expected %v, got %v", iso, apod.Now())
+  }
+}
